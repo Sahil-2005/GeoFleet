a6193a32a1641094374482aaef6e0527c923c38a
Sahil Gawade <104153594+Sahil-2005@users.noreply.github.com>
2026-03-14 15:19:50 +0530

    done with postgres setup in local machine

--- frontend/src/components/common/ProtectedRoute.jsx
+++ frontend/src/components/common/ProtectedRoute.jsx
@@ -1,5 +1,7 @@
 import { Navigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+// import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
+
 
 /**
  * Wraps a route to allow only specific roles.
